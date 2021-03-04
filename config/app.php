30a313549bc1c30ffb0876efc7a8dfa08f9ff697
apatiu <apatiu@gmail.com>
2021-03-04 23:43:01 +0700

    laravel permission

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -176,6 +177,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+
 
     ],
 
