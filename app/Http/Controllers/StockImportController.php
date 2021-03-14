9b4f80ad039459959a7062fd549397aef9f140ea
apatiu <apatiu@gmail.com>
2021-03-14 10:09:46 +0700

    payreceive table

---
+++ app/Http/Controllers/StockImportController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StockImport;
+use Illuminate\Http\Request;
+
+class StockImportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\StockImport  $stockImport
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StockImport $stockImport)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\StockImport  $stockImport
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StockImport $stockImport)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\StockImport  $stockImport
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StockImport $stockImport)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\StockImport  $stockImport
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StockImport $stockImport)
+    {
+        //
+    }
+}
