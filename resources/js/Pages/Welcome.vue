60ea6734ac1a3b6ca05da682e2ea0c5c9820a627
apatiu <apatiu@gmail.com>
2021-05-30 23:19:38 +0700

    select customer

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,7 @@
 <template>
     <div
-        class="relative flex items-top justify-center min-h-screen bg-white sm:items-center sm:pt-0">
+        class="relative flex items-top justify-center min-h-screen bg-white sm:items-center sm:pt-0"
+    style="background-image: url('/images/bg-billboard.jpg')">
         <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             <inertia-link v-if="$page.props.user" href="/dashboard" class="text-sm text-gray-700 underline">
                 Dashboard
